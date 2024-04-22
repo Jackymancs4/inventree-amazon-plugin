126901f29c0139c4a67cb1494118d1cee9fe45cc
Giacomo Rossetto <jackyman_cs4@live.it>
2024-04-22 23:10:17 +0200

    Run black

--- inventree_amazon_plugin/amazon.py
+++ inventree_amazon_plugin/amazon.py
@@ -3,7 +3,15 @@
 from decimal import Decimal
 from plugin import InvenTreePlugin
 from plugin.mixins import ActionMixin, APICallMixin, SettingsMixin, EventMixin
-from part.models import Part, SupplierPart, PartCategory, PartParameterTemplate, PartParameter, BomItem, BomItemSubstitute
+from part.models import (
+    Part,
+    SupplierPart,
+    PartCategory,
+    PartParameterTemplate,
+    PartParameter,
+    BomItem,
+    BomItemSubstitute,
+)
 from company.models import Company
 from order.models import Order, OrderLineItem, PurchaseOrder, PurchaseOrderLineItem
 from datetime import datetime
@@ -12,16 +20,17 @@ from zipfile import ZipFile
 import base64
 import csv
 
+
 class ImportAmazonOrdersPlugin(ActionMixin, SettingsMixin, InvenTreePlugin):
     """An EXTREMELY simple action plugin which demonstrates the capability of the ActionMixin class."""
 
-    NAME = 'ImportAmazonOrdersPlugin'
-    SLUG = 'amazon'
-    ACTION_NAME = 'amazon'
+    NAME = "ImportAmazonOrdersPlugin"
+    SLUG = "amazon"
+    ACTION_NAME = "amazon"
 
     def get_filenames(self, path_to_zip):
-        """ return list of filenames inside of the zip folder"""
-        with ZipFile(path_to_zip, 'r') as zip:
+        """return list of filenames inside of the zip folder"""
+        with ZipFile(path_to_zip, "r") as zip:
             return zip.namelist()
 
     def process_order_history(self, data, supplier):
@@ -37,9 +46,9 @@ class ImportAmazonOrdersPlugin(ActionMixin, SettingsMixin, InvenTreePlugin):
             order_id = row[1]
             order_date = datetime.fromisoformat(row[2])
 
-            part_name = (row[23][:50] + '..') if len(row[23]) > 50 else row[23]
+            part_name = (row[23][:50] + "..") if len(row[23]) > 50 else row[23]
             part_code = row[12]
-            part_description = row[23] if len(row[23]) > 50 else ''
+            part_description = row[23] if len(row[23]) > 50 else ""
             part_quantity = row[14]
             part_total_price = row[9]
             part_price_currency = row[4]
@@ -58,14 +67,12 @@ class ImportAmazonOrdersPlugin(ActionMixin, SettingsMixin, InvenTreePlugin):
                 order = order_map[order_id]
 
             part = Part.objects.get_or_create(
-                name = part_name,
-                description = part_description,
+                name=part_name,
+                description=part_description,
             )[0]
 
             supplier_part = SupplierPart.objects.get_or_create(
-                part = part,
-                supplier = supplier,
-                SKU = part_code
+                part=part, supplier=supplier, SKU=part_code
             )[0]
 
             supplier_part.link = "https://" + row[0] + "/dp/" + part_code
@@ -74,21 +81,23 @@ class ImportAmazonOrdersPlugin(ActionMixin, SettingsMixin, InvenTreePlugin):
             # order.add_line_item(supplier_part,)
 
             order_line_item = PurchaseOrderLineItem.objects.get_or_create(
-                order = order,
-                part = supplier_part,
-                quantity = part_quantity,
-                purchase_price_currency = part_price_currency
+                order=order,
+                part=supplier_part,
+                quantity=part_quantity,
+                purchase_price_currency=part_price_currency,
             )[0]
 
-            order_line_item.purchase_price = Decimal(part_total_price) / (1 if Decimal(part_quantity) == 0.0 else Decimal(part_quantity))
+            order_line_item.purchase_price = Decimal(part_total_price) / (
+                1 if Decimal(part_quantity) == 0.0 else Decimal(part_quantity)
+            )
             order_line_item.save()
 
-        print(f'Processed {line_count} lines.')
+        print(f"Processed {line_count} lines.")
         line_count += 1
 
         return order_map
 
-    def place_orders(self, data, user, default_location = None):
+    def place_orders(self, data, user, default_location=None):
 
         line_count = 0
 
@@ -99,39 +108,41 @@ class ImportAmazonOrdersPlugin(ActionMixin, SettingsMixin, InvenTreePlugin):
             pending_lines = data[order].pending_line_items()
 
             for pending_line in pending_lines:
-                data[order].receive_line_item(pending_line, default_location, pending_line.quantity, user)
+                data[order].receive_line_item(
+                    pending_line, default_location, pending_line.quantity, user
+                )
 
-        print(f'Processed {line_count} lines.')
+        print(f"Processed {line_count} lines.")
         line_count += 1
 
-    def complete_orders(self, data, user, default_location = None):
+    def complete_orders(self, data, user, default_location=None):
 
         line_count = 0
         for order in data:
 
             data[order].complete_order()
 
-        print(f'Processed {line_count} lines.')
+        print(f"Processed {line_count} lines.")
         line_count += 1
 
     def perform_action(self, user=None, data=None):
         """Sample method."""
-        print('Action plugin in action!')
+        print("Action plugin in action!")
 
         zip_temp_file = NamedTemporaryFile(delete=True)
 
         zip_temp_file_map = {}
 
-        zip_temp_file.write(base64.b64decode(data['aaa']))
+        zip_temp_file.write(base64.b64decode(data["aaa"]))
 
         print(zip_temp_file.name)
 
         company = Company.objects.get_or_create(
-            name = "Amazon",
-            is_supplier = True,
+            name="Amazon",
+            is_supplier=True,
         )[0]
 
-        with ZipFile(zip_temp_file.name, 'r') as zip:
+        with ZipFile(zip_temp_file.name, "r") as zip:
 
             for zip_file_name in zip.namelist():
                 zip_file_content = zip.read(zip_file_name)
@@ -139,9 +150,13 @@ class ImportAmazonOrdersPlugin(ActionMixin, SettingsMixin, InvenTreePlugin):
                 zip_temp_file_map[zip_file_name] = NamedTemporaryFile(delete=True)
                 zip_temp_file_map[zip_file_name].write(zip_file_content)
 
-                if(zip_file_name == 'Retail.OrderHistory.2/Retail.OrderHistory.2.csv') :
+                if zip_file_name == "Retail.OrderHistory.2/Retail.OrderHistory.2.csv":
 
-                    csv_reader = csv.reader(zip_file_content.decode().splitlines(), delimiter=",", lineterminator="\n")
+                    csv_reader = csv.reader(
+                        zip_file_content.decode().splitlines(),
+                        delimiter=",",
+                        lineterminator="\n",
+                    )
 
                     orders = self.process_order_history(csv_reader, company)
 
@@ -151,7 +166,7 @@ class ImportAmazonOrdersPlugin(ActionMixin, SettingsMixin, InvenTreePlugin):
 
     def get_info(self, user, data=None):
         """Sample method."""
-        return {'user': user.username, 'hello': 'world'}
+        return {"user": user.username, "hello": "world"}
 
     def get_result(self, user=None, data=None):
         """Sample method."""
